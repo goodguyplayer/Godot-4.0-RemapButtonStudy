1e7211ef1a09b9af639e685b2b7745d4308944ae
goodguyplayer <nathanbritodasilva@gmail.com>
2023-03-02 19:33:32 -0300

    action_remap_button - updated code

--- Menu/action_remap_button.gd
+++ Menu/action_remap_button.gd
@@ -8,7 +8,27 @@ func _ready():
 	set_process_unhandled_key_input(false)
 	display_current_key()
 	
+	
+func _toggled(button_pressed):
+	set_process_unhandled_key_input(button_pressed)
+	if button_pressed:
+		text = "... Key"
+		release_focus()
+	else:
+		display_current_key()
 
+
+func _unhandled_key_input(event):
+	# Note that you can use the _input callback instead, especially if
+	# you want to work with gamepads.
+	remap_action_to(event)
+	button_pressed = false
+	
+
+func remap_action_to(event):
+	InputMap.action_erase_events(action)
+	InputMap.action_add_event(action, event)
+	text = "%s Key" % event.as_text()
 
 
 func display_current_key():
