3a3179ed7c9edfec27c4a6dbfd44fe6af0f70eb9
goodguyplayer <nathanbritodasilva@gmail.com>
2023-03-03 16:38:26 -0300

    Moved to its own folders again

--- Menu/remappers/action_remap_button_origin/action_remap_button_origin.gd
+++ Menu/remappers/action_remap_button_origin/action_remap_button_origin.gd
@@ -6,13 +6,13 @@ extends Button
 func _ready():
 	assert(InputMap.has_action(action))
 	set_process_unhandled_key_input(false)
-	set_process_unhandled_input(false)
+#	set_process_unhandled_input(false)
 	display_current_key()
 	
 	
 func _toggled(button_pressed):
 	set_process_unhandled_key_input(button_pressed)
-	set_process_unhandled_input(button_pressed)
+#	set_process_unhandled_input(button_pressed)
 	if button_pressed:
 		text = "... Key"
 		release_focus()
@@ -23,17 +23,17 @@ func _toggled(button_pressed):
 # "For gameplay input, _unhandled_input and _unhandled_key_input are usually a better 
 # fit as they allow the GUI to intercept the events first." -
 # https://docs.godotengine.org/en/stable/classes/class_node.html#class-node-method-input
-func _unhandled_input(event):
-	if event is InputEventKey:
-		print(event.as_text_key_label())
-	
-	# Seems that it needs to be on the container containing the button?
-	if event is InputEventMouseButton:
-		print(InputMap.action_get_events(action)[1])
-		print(event.as_text())
-		
-	if event is InputEventJoypadButton:
-		print(event.as_text())
+#func _unhandled_input(event):
+#	if event is InputEventKey:
+#		print(event.as_text_key_label())
+#
+#	# Seems that it needs to be on the container containing the button?
+#	if event is InputEventMouseButton:
+#		print(InputMap.action_get_events(action)[1])
+#		print(event.as_text())
+#
+#	if event is InputEventJoypadButton:
+#		print(event.as_text())
 
 
 # "This method also performs better than _unhandled_input, since unrelated events such as
